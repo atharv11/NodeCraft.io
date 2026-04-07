166d991a1676c5a7c7afa60bae892086f74b3595
Hrushikesh Ahire <ahirehrushikesh016@gmail.com>
2026-04-07 23:32:49 +0200

    Code

--- src/App.tsx
+++ src/App.tsx
@@ -375,6 +375,7 @@ function FlowContent({
         selectionOnDrag={!isDragMode}
   
         //  visually change the cursor
+        // code 
         style={{ cursor: isDragMode ? 'grab' : 'crosshair' }}
   
       >
